c0768c946b9fd1019babcea0210c34ea961efc24
Spitball1234 <liam.jbm2020@gmail.com>
2025-09-01 17:52:06 -0400

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,25 @@
+const Main = imports.ui.main;
+
+let dashOriginalParent;
+
+function init() {
+}
+
+function enable() {
+    // This function is called when the extension is enabled.
+    // We save the dash's original parent and then remove it.
+    if (Main.overview.dash) {
+        dashOriginalParent = Main.overview.dash.get_parent();
+        if (dashOriginalParent) {
+            Main.overview.dash.hide();
+        }
+    }
+}
+
+function disable() {
+    // This function is called when the extension is disabled.
+    // We restore the dash to its original state.
+    if (Main.overview.dash) {
+        Main.overview.dash.show();
+    }
+}
