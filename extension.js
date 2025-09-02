709ea9eb727e4d4a7e14fe6010d13b1b2efe294a
Spitball1234 <liam.jbm2020@gmail.com>
2025-09-02 08:34:54 -0400

    More gnome shell extension new version changes

--- extension.js
+++ extension.js
@@ -1,25 +1,31 @@
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 let dashOriginalParent;
 
-function init() {
-}
+export default class MyExtension extends Extension {
+    // The constructor is optional but can be used for initialization.
+    // It's good practice to call super(metadata) to properly initialize the parent class.
+    constructor(metadata) {
+        super(metadata);
+    }
 
-function enable() {
-    // This function is called when the extension is enabled.
-    // We save the dash's original parent and then remove it.
-    if (Main.overview.dash) {
-        dashOriginalParent = Main.overview.dash.get_parent();
-        if (dashOriginalParent) {
-            Main.overview.dash.hide();
+    enable() {
+        // This function is called when the extension is enabled.
+        // We save the dash's original parent and then remove it.
+        if (Main.overview.dash) {
+            dashOriginalParent = Main.overview.dash.get_parent();
+            if (dashOriginalParent) {
+                Main.overview.dash.hide();
+            }
         }
     }
-}
 
-function disable() {
-    // This function is called when the extension is disabled.
-    // We restore the dash to its original state.
-    if (Main.overview.dash) {
-        Main.overview.dash.show();
+    disable() {
+        // This function is called when the extension is disabled.
+        // We restore the dash to its original state.
+        if (Main.overview.dash) {
+            Main.overview.dash.show();
+        }
     }
 }
